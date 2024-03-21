18b291ca0df48e1adc98d0134241b31c67603c7b
jumiray <jumiray620@gmail.com>
2024-03-21 20:38:18 +0800

    fix: style

--- pages/monitoring/detail.vue
+++ pages/monitoring/detail.vue
@@ -244,10 +244,8 @@ export default {
 
     .right-content {
       padding-left: 20px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-
+      flex: 1;
+      text-align: left;
       .author {
         color: rgba(255, 255, 255, 0.60);
         font-family: Avenir;
