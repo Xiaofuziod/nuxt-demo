fe90864f0d6aebe8e051310c4096785309b327ea
flynn998 <flynn.taurion@gmail.com>
2024-03-24 23:32:32 +0800

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,7 +81,7 @@ export default {
       {code: 'zh', iso: 'zh', file: 'zh.js'},
       {code: 'pt-br', iso: 'pt-br', file: 'pt-br.js'},
     ],
-    defaultLocale: 'zh',
+    defaultLocale: 'en',
     lazy: true,
     langDir: 'lang/',
     silentTranslationWarn: true,
