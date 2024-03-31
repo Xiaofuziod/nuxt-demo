d94ce64e6b09b98c6326da789b718eb44c30243a
jumiray <jumiray620@gmail.com>
2024-03-31 20:20:55 +0800

    fix:am

--- utils/formatTimeBasedOnRule.js
+++ utils/formatTimeBasedOnRule.js
@@ -1,17 +1,18 @@
-export function formatTimeBasedOnRule(utcTimeString, today) {
+function formatTimeBasedOnRule(utcTimeString, today) {
     const utcDate = new Date(utcTimeString + 'Z');
 
+    // Get the current date and time
     const now = new Date();
 
-    const currentDate = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate());
-    const targetDate = new Date(utcDate.getUTCFullYear(), utcDate.getUTCMonth(), utcDate.getUTCDate());
+    const currentDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const targetDate = new Date(utcDate.getFullYear(), utcDate.getMonth(), utcDate.getDate());
 
     let formattedTime;
 
     if (currentDate.getTime() === targetDate.getTime()) {
-        formattedTime = today + utcDate.getUTCHours().toString().padStart(2, '0') + ':' + utcDate.getUTCMinutes().toString().padStart(2, '0');
+        formattedTime = today + utcDate.getHours().toString().padStart(2, '0') + ':' + utcDate.getMinutes().toString().padStart(2, '0');
     } else {
-        formattedTime = (utcDate.getUTCMonth() + 1).toString().padStart(2, '0') + '/' + utcDate.getUTCDate().toString().padStart(2, '0') + ' ' + utcDate.getUTCHours().toString().padStart(2, '0') + ':' + utcDate.getUTCMinutes().toString().padStart(2, '0');
+        formattedTime = (utcDate.getMonth() + 1).toString().padStart(2, '0') + '/' + utcDate.getDate().toString().padStart(2, '0') + ' ' + utcDate.getHours().toString().padStart(2, '0') + ':' + utcDate.getMinutes().toString().padStart(2, '0');
     }
 
     return formattedTime;
