3b99d922be17fcba844e79d8a141e43213f0a140
flynn998 <flynn.taurion@gmail.com>
2024-03-31 22:19:45 +0800

    up

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,16 +1,3 @@
-// // plugins/socket.io.js
-//
-// import io from 'socket.io-client';
-//
-// const token = localStorage.getItem('token')
-// const socket = io("https://api-test.taurion.ai?token=" + token, {
-//   reconnectionAttempts: 50,
-// }); // 确保这里的 URL 指向你的服务端地址
-//
-// export default (context, inject) => {
-//   inject('socket', socket);
-// };
-//
 
 // plugins/socket.js
 import Vue from 'vue';
