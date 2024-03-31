d13aaf0e447ffb334b207cd74aad637594b4d7f6
flynn998 <flynn.taurion@gmail.com>
2024-03-31 11:56:29 +0800

    修改接口地址

--- common/home.js
+++ common/home.js
@@ -1,5 +1,5 @@
 
-const baseUrl = process.env.NODE_ENV === 'development' ? 'api' : 'https://api-test.taurion.ai'
+const baseUrl = process.env.NODE_ENV === 'development' ? 'api' : 'https://54.207.113.59'
 export const sendEmail = baseUrl + '/api/auth/base/sendMessage'
 export const userLogin = baseUrl + '/api/auth/login/dologin'
 export const userRegister = baseUrl + '/api/auth/login/register'
